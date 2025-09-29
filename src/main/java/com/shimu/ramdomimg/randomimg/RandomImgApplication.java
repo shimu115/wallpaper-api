b7363cf1cb085784dc5ba95add29cdb1927081d2
shimu <shimuspace@gmail.com>
2025-09-29 15:56:49 +0800

    init project.

---
+++ src/main/java/com/shimu/ramdomimg/randomimg/RandomImgApplication.java
@@ -0,0 +1,13 @@
+package com.shimu.ramdomimg.randomimg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomImgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomImgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shimu/ramdomimg/randomimg/RandomImgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shimu.ramdomimg.randomimg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomImgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
