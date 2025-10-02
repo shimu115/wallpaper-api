ca7dd7c080eee4837bb5905e9c1ddb6db62575b4
shimu <shimuspace@gmail.com>
2025-10-02 19:15:15 +0800

    github 的 json 数据地址更换为国内镜像源地址，以防访问不了 github 导致接口超时

--- src/main/java/com/shimu/ramdomimg/enums/ApiContains.java
+++ src/main/java/com/shimu/ramdomimg/enums/ApiContains.java
@@ -5,5 +5,5 @@ public class    ApiContains {
     // https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN
     public static String BING_PHOTO_API = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN";
     // 文件位置：https://github.com/flow2000/bing-wallpaper-api/blob/master/data/zh-CN_all.json
-    public static String BING_GITHUB_JSON = "https://raw.githubusercontent.com/flow2000/bing-wallpaper-api/refs/heads/master/data/{i18n_key}.json";
+    public static String BING_GITHUB_JSON = "https://hub.gitmirror.com/raw.githubusercontent.com/flow2000/bing-wallpaper-api/refs/heads/master/data/{i18n_key}.json";
 }
