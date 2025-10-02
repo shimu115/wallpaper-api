a0bbe8cef995106206a33be609f55609f4572616
shimu <shimuspace@gmail.com>
2025-10-02 17:50:13 +0800

    添加启动成功的日志打印

--- src/main/java/com/shimu/ramdomimg/RandomImgApplication.java
+++ src/main/java/com/shimu/ramdomimg/RandomImgApplication.java
@@ -1,13 +1,16 @@
 package com.shimu.ramdomimg;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class RandomImgApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RandomImgApplication.class, args);
+        log.info("************* 启动成功 *************");
     }
 
 }
