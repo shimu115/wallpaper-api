294fb3151ab649a8f5ee65f154c5f3756ff81d69
shimu <shimuspace@gmail.com>
2025-10-02 13:43:21 +0800

    修改方法名，此 Bing 接口为每日壁纸接口，重新定义随机图片接口；修改 getTodayWallpaper 接口的逻辑，手动捕捉异常

--- src/main/java/com/shimu/ramdomimg/controlloer/BingController.java
+++ src/main/java/com/shimu/ramdomimg/controlloer/BingController.java
@@ -1,6 +1,7 @@
 package com.shimu.ramdomimg.controlloer;
 
 import com.shimu.ramdomimg.enums.ApiContains;
+import com.shimu.ramdomimg.exception.RandomImgException;
 import com.shimu.ramdomimg.model.response.BingResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -25,8 +26,13 @@ public class BingController {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    @GetMapping("/getRandomImage")
-    public void getRandomImage(HttpServletResponse response) throws IOException {
+    /**
+     * bing 每日壁纸接口
+     * @param response (无需传参)
+     * @throws IOException
+     */
+    @GetMapping("/getTodayWallpaper")
+    public void getTodayWallpaper(HttpServletResponse response) {
         // 1. 请求 Bing 官方 JSON 接口
         String bingApi = ApiContains.BING_PHOTO_API;
         RestTemplate restTemplate = new RestTemplate();
@@ -35,12 +41,16 @@ public class BingController {
         // 2. 拼接完整图片地址
         assert bing != null;
         String imageUrl = "https://www.bing.com" + bing.getImages().get(0).getUrl();
-        HttpURLConnection conn = (HttpURLConnection) new URL(imageUrl).openConnection();
-//        conn.setRequestProperty("User-Agent", "Java"); // 可选
-        int status = conn.getResponseCode();
-        if (status != HttpURLConnection.HTTP_OK) {
-            response.sendError(HttpServletResponse.SC_BAD_GATEWAY);
-            return;
+        HttpURLConnection conn = null;
+        try {
+            conn = (HttpURLConnection) new URL(imageUrl).openConnection();
+            int status = conn.getResponseCode();
+            if (status != HttpURLConnection.HTTP_OK) {
+                response.sendError(HttpServletResponse.SC_BAD_GATEWAY);
+                return;
+            }
+        } catch (IOException e) {
+            throw new RandomImgException(e);
         }
 
         String contentType = conn.getContentType();
@@ -58,8 +68,16 @@ public class BingController {
                 out.write(buffer, 0, len);
             }
             out.flush();
+        } catch (IOException e) {
+            throw new RandomImgException(e);
         } finally {
             conn.disconnect();
         }
+    }
+
+    // TODO bing 随机壁纸接口
+    @GetMapping("getRandomImage")
+    public void getRandomImage(HttpServletResponse response) throws IOException {
+
     }
 }
